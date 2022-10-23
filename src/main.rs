9de080a7ea284de38efa1d386f94860e691b693f
Daniel Garcia <danielgarcia@nkbr.email>
2022-10-23 14:40:17 -0500

    backbone without implementation

---
+++ src/main.rs
@@ -0,0 +1,22 @@
+//Author:   Daniel Garcia
+//date:     23OCT22
+//ver:      0.1.0-alpha
+// DESC: To be used to copy save files for Grim Dawn from Documents location to
+//       Steam remote location as well as vice versa.
+
+
+fn main() {
+    println!("Hello, world!");
+
+    //discover Steam\userdata\XXX\219990\remote\save
+
+    //discover Documents\My Games\Grim Dawn\save
+}
+
+fn copy_mydoc_to_steam(){
+
+}
+
+fn copy_steam_to_mydoc(){
+
+}
