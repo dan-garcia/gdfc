fe989a819305ac1c13a7e73dbdecabd317e9ccd0
Daniel Garcia <danielgarcia@nkbr.email>
2022-12-26 14:34:48 -0600

    Update deps

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
 //Author:   Daniel Garcia
 //date:     23OCT22
 //ver:      0.1.0-alpha
-// DESC: To be used to copy save files for Grim Dawn from Documents location to
-//       Steam remote location and vice versa.
+// DESC:    To be used to copy save files for Grim Dawn from Documents location to
+//          Steam remote location and vice versa.
+// DEPS:    steamlocate 0.*
+//          steamid-ng  1.*
 
+use std::path::Path;
 
 fn main() {
     println!("Hello, world!");
