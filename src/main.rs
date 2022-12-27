fdffe6d65245b257cebfa46d976904575858c900
Daniel Garcia <danielgarcia@nkbr.email>
2022-12-26 18:37:29 -0600

    Impl both steam save dirs

--- src/main.rs
+++ src/main.rs
@@ -5,15 +5,18 @@
 //          Steam remote location and vice versa.
 // DEPS:    steamlocate 0.*
 //          steamid-ng  1.*
-//          dirs-next   2.*
+//          dirs_next   2.*
 
 // https://docs.rs/steamlocate/latest/steamlocate/
 extern crate steamlocate;
 // https://docs.rs/steamid-ng/latest/steamid_ng/
 extern crate steamid_ng;
+// https://docs.rs/dirs-next/latest/dirs_next/
+extern crate dirs_next;
 use std::path::PathBuf;
 use steamlocate::SteamDir;
 use steamid_ng::SteamID;
+use dirs_next::document_dir;
 
 fn main()
 {
@@ -22,9 +25,6 @@ fn main()
     //TODO: fix initial formatting to match expected rust formatting
     //      at top of file
 
-    //discover Steam\userdata\XXX\219990\remote\save
-    //discover Documents\My Games\Grim Dawn\save
-
     //check if Steam install exists
     match SteamDir::locate()
     {
@@ -32,31 +32,35 @@ fn main()
         None => panic!("No Steam install found")
     }
 
-    let mut steam_dir = SteamDir::locate().unwrap();
-    let install_dir = &steam_dir.path;
-    dbg!(&install_dir);
-    // need to take in steam3 (or steamid or steam2) as an arg
-    // and parse appropriately to pass into steamid
-    let steam3_id : SteamID = SteamID::from_steam3("[U:1:6620387]").unwrap();
-    let steam_account_id = SteamID::account_id(&steam3_id);
-    dbg!(steam_account_id);
+    let mut steam_dir : SteamDir = SteamDir::locate().unwrap();
 
-    // assemble path for remote save directory
-    let mut steam_remote_save_dir : PathBuf = install_dir.to_path_buf();
-    steam_remote_save_dir.push("userdata\\");
-    steam_remote_save_dir.push(steam_account_id.to_string()+"\\");
-    steam_remote_save_dir.push("219990\\remote\\save\\");
-
-    // assemble path for windows save directory
-    let mut mydoc_save_dir : PathBuf = PathBuf::from("");
-
-    dbg!(steam_remote_save_dir);
     //check if Grim Dawn is installed
-    match steam_dir.app(&219990)
+    match &steam_dir.app(&219990)
     {
         Some(_) => (),
         None => panic!("No Grim Dawn install directory found")
     }
+
+    // FIXME need to take in steam3 (or steamid or steam2) as an arg
+    // and parse appropriately to pass into SteamID::from_*
+    // as well as change to unwrap_or() or unwrap_or_*()
+    let steam3_id : SteamID = SteamID::from_steam3("[U:1:6620387]").unwrap();
+    let steam_account_id : u32 = SteamID::account_id(&steam3_id);
+    dbg!(steam_account_id);
+
+    // assemble path for remote save directory
+    let steam_install_dir : &PathBuf = &steam_dir.path;
+    let mut steam_remote_save_dir : PathBuf = steam_install_dir.to_path_buf();
+    steam_remote_save_dir.push("userdata\\");
+    steam_remote_save_dir.push(steam_account_id.to_string()+"\\");
+    steam_remote_save_dir.push("219990\\remote\\save\\");
+    dbg!(steam_remote_save_dir);
+
+    // assemble path for windows steam non-remote save directory
+    let mut steam_mydoc_save_dir : PathBuf = document_dir().unwrap_or_default();
+    steam_mydoc_save_dir.push("\\");
+    steam_mydoc_save_dir.push("My Games\\Grim Dawn\\save\\");
+    dbg!(steam_mydoc_save_dir);
 }
 
 fn copy_mydoc_to_steam()
