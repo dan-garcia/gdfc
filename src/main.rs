a665afbb3c92afb36df916eb7e0789cc8776745e
Daniel Garcia <danielgarcia@nkbr.email>
2023-01-05 16:48:10 -0600

    Partial impl of arg parser

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
 // Author:  Daniel Garcia
-// date:    26DEC22
 // ver:     0.1.1-alpha
-// DESC:    To be used to copy save files for Grim Dawn from Documents location to
-//          Steam remote location and vice versa.
-// DEPS:    steamlocate 0.*
-//          steamid-ng  1.*
-//          dirs_next   2.*
+// DESC:    Simple CLI tool to transfer Grim Dawn save files from Steam's remote 
+//          save directory to the default save directory and vice-versa
+
+
+//  TODO:   fix formatting to match expected rust formatting
+//          both for fns and top of file
 
 // https://docs.rs/steamlocate/latest/steamlocate/
 extern crate steamlocate;
@@ -13,35 +13,54 @@ extern crate steamlocate;
 extern crate steamid_ng;
 // https://docs.rs/dirs-next/latest/dirs_next/
 extern crate dirs_next;
+// https://docs.rs/clap/latest/clap/
+extern crate clap;
+
 use std::path::PathBuf;
 use steamlocate::{SteamDir, SteamApp};
 use steamid_ng::SteamID;
 use dirs_next::document_dir;
+use clap::Parser;
 
-//  TODO:   fix formatting to match expected rust formatting
-//          both for fns and top of file
+// FIXME add in tags for proper arg flags
+#[derive(Parser)]
+// FIXME about and author are not being picked up here?
+#[command(author, version, about, long_about = None)]
+struct Args
+{
+    // SteamID3 as string
+    steamid3 : String,
+    #[arg(short, long)]
+    debug : bool,
+}
 
 fn main() -> Result<(), Box<dyn std::error::Error>>
 {
-    //check if Steam install exists
+    // parse args w/clap
+    let passed_args = Args::parse();
+
+    // FIXME need proper validation of steamid3 passed from args
+    // currently just defaults to a bogus ID
+    let steam_id : SteamID = SteamID::from_steam3(&passed_args.steamid3)
+                                    .unwrap_or(SteamID::from_steam3("[U:1:1234567]").unwrap());
+    dbg!(&steam_id);
+    let steam_account_id : u32 = SteamID::account_id(&steam_id);
+    dbg!(&steam_account_id);
+
+    // check if Steam install exists
     if get_steam_dir().is_none()
     {
+        // FIXME we should handle this with more grace
         panic!("No Steam install found")
     }
     let mut steam_dir : SteamDir = get_steam_dir().unwrap();
 
-    //check if Grim Dawn is installed
+    // check if Grim Dawn is installed
     if get_grim_dawn_dir(&mut steam_dir).is_none()
     {
+        // FIXME we should handle this with more grace
         panic!("No Grim Dawn install directory found");
     }
-
-    // FIXME need to take in steam3 (or steamid or steam2) as an arg
-    // and parse appropriately to pass into SteamID::from_*
-    // as well as change to unwrap_or() or unwrap_or_*()
-    let steam3_id : SteamID = SteamID::from_steam3("[U:1:6620387]").unwrap();
-    let steam_account_id : u32 = SteamID::account_id(&steam3_id);
-    dbg!(&steam_account_id);
 
     // assemble path for remote save directory
     let steam_install_dir : &PathBuf = &steam_dir.path;
@@ -54,6 +73,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>>
     // assemble path for windows steam non-remote save directory
     if get_docs_dir().is_none()
     {
+        // FIXME we should handle this with more grace
         panic!("No default \"Documents\" directory found");
     }
     let mut steam_mydoc_save_dir : PathBuf = PathBuf::new();
@@ -65,6 +85,9 @@ fn main() -> Result<(), Box<dyn std::error::Error>>
 
 }
 
+/// Returns Option\<std::path::PathBuf\> of default 'documents' directory.
+/// 
+/// https://docs.rs/dirs-next/latest/dirs_next/
 fn get_docs_dir() -> Option<PathBuf>
 {
     match document_dir()
@@ -74,6 +97,9 @@ fn get_docs_dir() -> Option<PathBuf>
     }
 }
 
+/// Returns Option\<Steamlocate::SteamDir\> of Steam install directory.
+/// 
+/// https://docs.rs/steamlocate/latest/steamlocate/
 fn get_steam_dir() -> Option<SteamDir>
 {
     match SteamDir::locate()
@@ -83,6 +109,9 @@ fn get_steam_dir() -> Option<SteamDir>
     }
 }
 
+/// Returns Option\<Steamlocate::SteamApp\> of Grim Dawn install directory.
+/// 
+/// https://docs.rs/steamlocate/latest/steamlocate/
 // we need a mutable ref here because steamlocate::SteamDir.app()
 // takes a &mut
 fn get_grim_dawn_dir(sdir : &mut SteamDir) -> Option<SteamApp>
@@ -94,12 +123,12 @@ fn get_grim_dawn_dir(sdir : &mut SteamDir) -> Option<SteamApp>
     }
 }
 
-fn copy_mydoc_to_steam()
-{
+// fn copy_docs_to_steam()
+// {
 
-}
+// }
 
-fn copy_steam_to_mydoc()
-{
+// fn copy_steam_to_docs()
+// {
 
-}
+// }
